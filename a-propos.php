73a5e89a02a2e787297109bf007058d7b3d70a31
Amrr-ai <291039214+Amrr-ai@users.noreply.github.com>
2026-06-22 15:35:30 +0200

    refactor: simplify title on about page

--- a-propos.php
+++ a-propos.php
@@ -12,7 +12,7 @@ require_once('./ressources/includes/connexion-bdd.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>À propos - BUT MMI - SAÉ 203</title>
+    <title>A propos - SAÉ 203</title>
 
     <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/reset.css">
     <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/fonts.css">
