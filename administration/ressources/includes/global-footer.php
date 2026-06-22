54fd27b3623a826718c5e3cc369889af95028237
Amrr-ai <291039214+Amrr-ai@users.noreply.github.com>
2026-06-22 15:35:29 +0200

    style: replace template text block with structured list in administration footer

--- administration/ressources/includes/global-footer.php
+++ administration/ressources/includes/global-footer.php
@@ -1,11 +1,15 @@
 <?php
-    if (!is_null($mysqli_link)) {
-        mysqli_close($mysqli_link);
+    if (isset($page_courante) && $page_courante == "index") {
+        $mysqli_link->close();
     }
 ?>
 <footer class="border-y border-gray-400 text-center mx-6 py-2 mb-1">
-    <p class="font-bold text-gray-900">SAÉ 203 - Concevoir un site web avec une source de données</p>
-    <p class="font-bold text-gray-900 uppercase">MMI <?php echo (date("Y") - 1) . "-" . (date("Y") + 2); ?></p>
-    <p class="text-gray-700">Projet réalisé par :</p>
-    <p class="text-gray-800 font-medium">Ammar J. Nom_Equipier_2 Nom_Equipier_3</p>
+    <p class="font-bold">SAÉ 203 - Concevoir un site web avec une source de données</p>
+    <p class="font-bold">MMI <?php echo (date("Y") - 1) . "-" . (date("Y") + 2); ?></p>
+    <p>Projet réalisé par :</p>
+    <ul class="inline-flex">
+        <li class="px-1">Ammar J.</li>
+        <li class="px-1">Nom_Equipier_2</li>
+        <li class="px-1">Nom_Equipier_3</li>
+    </ul>
 </footer>
