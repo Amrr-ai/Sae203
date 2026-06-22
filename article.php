6543e7b131f2aeabb06f03d4f6d95135681c5d2e
Amrr-ai <291039214+Amrr-ai@users.noreply.github.com>
2026-06-22 15:35:29 +0200

    feat: add CYU favicon to article view page

--- article.php
+++ article.php
@@ -35,6 +35,7 @@ if (!$entite) {
 <html lang="fr">
 <head>
     <base href="/<?php echo $_ENV['CHEMIN_BASE']; ?>">
+    <link rel="icon" href="./ressources/images/logo-cyu-couleur.svg" type="image/svg+xml">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
