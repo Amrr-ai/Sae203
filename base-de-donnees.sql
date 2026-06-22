6c8667bd8f98ba0d2aa8547202f11f42bb46ce8f
Amrr-ai <291039214+Amrr-ai@users.noreply.github.com>
2026-06-22 16:44:29 +0200

    database: update base-de-donnees.sql with final populated contents

--- base-de-donnees.sql
+++ base-de-donnees.sql
@@ -1,117 +1,156 @@
-CREATE DATABASE  IF NOT EXISTS `sae_203_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `sae_203_db`;
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: sae_203_db
--- ------------------------------------------------------
--- Server version	8.0.29
+-- Host: mysql-saesql.alwaysdata.net
+-- Generation Time: Jun 22, 2026 at 04:42 PM
+-- Server version: 11.4.12-MariaDB
+-- PHP Version: 8.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `saesql_203`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `article`
 --
 
-DROP TABLE IF EXISTS `article`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `article` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `auteur_id` int DEFAULT NULL,
-  `titre` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `chapo` longtext CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `contenu` longtext CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `date_creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '(DC2Type:datetime_immutable)',
-  `lien_yt` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_23A0E6660BB6FE6` (`auteur_id`),
-  CONSTRAINT `FK_23A0E6660BB6FE6` FOREIGN KEY (`auteur_id`) REFERENCES `auteur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `auteur_id` int(11) DEFAULT NULL,
+  `titre` varchar(255) NOT NULL,
+  `chapo` longtext NOT NULL,
+  `contenu` longtext NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date_creation` datetime NOT NULL DEFAULT current_timestamp() COMMENT '(DC2Type:datetime_immutable)',
+  `lien_yt` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `article`
 --
 
-LOCK TABLES `article` WRITE;
-/*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (1,2,'Test 1445','Dolores voluptas sit aut.','Ut amet velit enim omnis error. Et dolorem et deserunt omnis et vitae. Ut et a nostrum eveniet.','https://via.placeholder.com/800x445.png/00eeff?text=consectetur','2023-06-14 20:53:00',''),(7,NULL,'Test','Test','Test ','https://via.placeholder.com/800x445.png/00eeff?text=consectetur','2023-05-09 13:32:06','');
-/*!40000 ALTER TABLE `article` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `article` (`id`, `auteur_id`, `titre`, `chapo`, `contenu`, `image`, `date_creation`, `lien_yt`) VALUES
+(1, 2, 'Mise en ligne du site web', 'Projet web dynamique réalisé dans le cadre de la SAÉ 203.', 'Ce site web a été réalisé dans le cadre de la SAÉ 203 afin de mettre en pratique la création d’un front-office et d’un back-office connectés à une base de données.\r\n\r\n', 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', '2026-07-19 20:53:00', ''),
+(2, 1, 'Développement de l’administration', 'Une administration pensée pour gérer les contenus du site.', 'La partie administration permet de gérer les contenus du site, notamment les articles et les auteurs, grâce aux formulaires et à la base de données.', 'https://images.unsplash.com/photo-1554098415-788601c80aef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', '2026-07-19 20:53:00', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `auteur`
 --
 
-DROP TABLE IF EXISTS `auteur`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `auteur` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `prenom` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `lien_twitter` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `lien_avatar` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `lien_twitter` varchar(255) NOT NULL,
+  `lien_avatar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `auteur`
 --
 
-LOCK TABLES `auteur` WRITE;
-/*!40000 ALTER TABLE `auteur` DISABLE KEYS */;
-INSERT INTO `auteur` VALUES (1,'Petit','A REMPLACER','A REMPLACER',''),(2,'Test man','A REMPLACER','',''),(3,'Thomas','Martin','https://twitter.com/universitecergy','https://www.cyu.fr/uas/cy/LOGO_FOOTER/CY-Cergy-Paris-Universite-Marianne.png'),(5,'Martin','Pauline','','https://www.cyu.fr/uas/cy/LOGO_FOOTER/CY-Cergy-Paris-Universite-Marianne.png');
-/*!40000 ALTER TABLE `auteur` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `auteur` (`id`, `nom`, `prenom`, `lien_twitter`, `lien_avatar`) VALUES
+(1, 'Osei-Mohamend', 'Ammar', 'https://twitter.com/universitecergy', 'https://pplx-res.cloudinary.com/image/upload/pplx_search_images/59349aabd4160ac20fa20db81448dfa5e2b0f947.jpg\r\n'),
+(2, 'Mbumba', 'Emmanuel', 'https://twitter.com/universitecergy', 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'),
+(3, 'Thomas', 'Martin', 'https://twitter.com/universitecergy', 'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png'),
+(4, 'Martin', 'Pauline', 'https://twitter.com/universitecergy', 'https://cdn.pixabay.com/photo/2016/03/31/17/33/avatar-1293744_1280.png'),
+(5, 'Dupont', 'Jean', 'https://twitter.com/universitecergy', 'https://cdn.pixabay.com/photo/2017/01/10/03/54/avatar-1968236_1280.png\r\n\r\n');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `message`
 --
 
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `message` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `prenom` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `contenu` longtext CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `type` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci NOT NULL,
-  `date_creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contenu` longtext NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `date_creation` datetime NOT NULL DEFAULT current_timestamp() COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `message`
 --
 
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,'Martin','Thomas','m.thomas43@yopmail.com','Je suis intéressé par la formation.','etudiant','2022-04-13 08:28:01'),(2,'Despoux','Helena','h.despoux@foo.fr','Je suis intéressé par la formation.','etudiant','2022-04-13 08:28:01');
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `message` (`id`, `nom`, `prenom`, `email`, `contenu`, `type`, `date_creation`) VALUES
+(1, 'Martin', 'Thomas', 'm.thomas43@yopmail.com', 'Je suis intéressé par la formation.', 'etudiant', '2022-04-13 08:28:01'),
+(2, 'Despoux', 'Helena', 'h.despoux@foo.fr', 'Je suis intéressé par la formation.', 'etudiant', '2022-04-13 08:28:01');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_23A0E6660BB6FE6` (`auteur_id`);
+
+--
+-- Indexes for table `auteur`
+--
+ALTER TABLE `auteur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `auteur`
+--
+ALTER TABLE `auteur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `article`
+--
+ALTER TABLE `article`
+  ADD CONSTRAINT `FK_23A0E6660BB6FE6` FOREIGN KEY (`auteur_id`) REFERENCES `auteur` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-16 20:41:53
